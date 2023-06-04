496f14044ff3c717d373fcc0d29c57fa166004ed
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-06-04 02:37:52 -0300

    style(web): change background and text colors

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -12,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-zinc-950 text-zinc-50`}>
+        {children}
+      </body>
     </html>
   )
 }
