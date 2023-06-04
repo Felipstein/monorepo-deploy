5dff8e8fe905e6288d30edb1f3ab70a13b99740c
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-06-04 02:46:38 -0300

    build: add cross-env to set node_env

--- apps/server/src/database/index.ts
+++ apps/server/src/database/index.ts
@@ -1,7 +1,8 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient({
-  log: ['query'],
-})
+const prisma =
+  process.env.NODE_ENV === 'development'
+    ? new PrismaClient({ log: ['query'] })
+    : new PrismaClient()
 
 export { prisma }
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,16 +1,12 @@
-import chalk from 'chalk'
-
 import { app } from './app'
 
 if (!process.env.JWT_SECRET) {
-  throw new Error(chalk.red('JWT_SECRET value not setted, aborting server...'))
+  throw new Error('JWT_SECRET value not setted, aborting server...')
 }
 
 app
   .listen({
     port: 3333,
   })
-  .then(() =>
-    console.info(chalk.green('HTTP server running on http://localhost:3333.')),
-  )
-  .catch((err) => console.error(chalk.red('Fail to run server:', err)))
+  .then(() => console.info('HTTP server running on http://localhost:3333.'))
+  .catch((err) => console.error('Fail to run server:', err))
